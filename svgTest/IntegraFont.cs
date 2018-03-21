f1d4ec359bf7d266ae497d711bad0c8049322cd5
Renan Toniolo Rocha <renantoniolo@gmail.com>
2018-03-21 07:56:21 -0300

    teste de commit
    teste de commit/push

--- svgTest/IntegraFont.cs
+++ svgTest/IntegraFont.cs
@@ -6,6 +6,7 @@ namespace svgTest
 {
     public class IntegraFont : IconModule
     {
+        //teste
         public IntegraFont() : base("Integra Icons", "fonte-integra-inline", "fonte-integra-inline.ttf", IntegraCollection.Icons)
         {
             
