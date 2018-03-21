93011c8063074a2ac436528d3d5f75977caf9690
Renan Toniolo Rocha <renantoniolo@gmail.com>
2018-03-21 08:01:09 -0300

    commit feature
    teste de feature

--- svgTest/IntegraFont.cs
+++ svgTest/IntegraFont.cs
@@ -6,7 +6,7 @@ namespace svgTest
 {
     public class IntegraFont : IconModule
     {
-        //teste
+        //teste 1234567
         public IntegraFont() : base("Integra Icons", "fonte-integra-inline", "fonte-integra-inline.ttf", IntegraCollection.Icons)
         {
             
